7cee9f40ce8e69a82b21d7f620ef5598efc68c3f
Gil-uai <aguiar.gilcimar@gmail.com>
2022-10-10 21:51:14 -0300

    Atualizado arquivo setup.py do projeto 2

--- CAP_02-Python/02_Projetos/Pacotes/IMAGE_PROCESSING_PACK/setup.py
+++ CAP_02-Python/02_Projetos/Pacotes/IMAGE_PROCESSING_PACK/setup.py
@@ -11,14 +11,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
-    version="0.0.1",
+    name="IMAGE_PROCESSING_PACK",
+    version="0.0.2",
     author="Gilcimar Gomes",
     author_email="aguiar.gilcimar@gmail.com",
     description="Version for test, processing to image - Project development by Karina Kato - teacher for DIO",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Gil-uai/DIO-Gera-o-Tech-Unimed/tree/main/CAP_02-Python/02_Projetos/Pacotes/IMAGE_PROCESSING_PACK",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
